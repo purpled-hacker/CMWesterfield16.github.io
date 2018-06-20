2d9b46c6f1bef3feb311627e5dbc955baf523fc8
Caitlin Westerfield <caitlin.westerfield@yale.edu>
2018-06-19 22:38:16 -0500

    first website

---
+++ README.md
@@ -0,0 +1,7 @@
+Caitlin Westerfield's Personal Website
+
+Starting Template: Start Bootstrap - Resume (https://startbootstrap.com/template-overviews/resume/)
+
+Also used are documents from the Start Bootstrap - Creativity and Start Bootstrap - 3 Column Portfolio code bases.
+
+All photos were either taken by Caitlin or were taken by photographers at the event who have given the organization who hosted the event the right to the use the photos. 
---
+++ main.js
@@ -0,0 +1,34 @@
+function SaveToDisk(fileURL, fileName) {
+    // for non-IE
+    if (!window.ActiveXObject) {
+        var save = document.createElement('a');
+        save.href = fileURL;
+        save.download = fileName || 'unknown';
+        save.style = 'display:none;opacity:0;color:transparent;';
+        (document.body || document.documentElement).appendChild(save);
+
+        if (typeof save.click === 'function') {
+            save.click();
+        } else {
+            save.target = '_blank';
+            var event = document.createEvent('Event');
+            event.initEvent('click', true, true);
+            save.dispatchEvent(event);
+        }
+
+        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    }
+
+    // for IE
+    else if (!!window.ActiveXObject && document.execCommand) {
+        var _window = window.open(fileURL, '_blank');
+        _window.document.close();
+        _window.document.execCommand('SaveAs', true, fileName || fileURL)
+        _window.close();
+    }
+}
+
+var btnResume = document.getElementById('btn-resume');
+btnResume.addEventListener('click', function() {
+  SaveToDisk('resume/resume.pdf', 'Caitlin-Westerfield.pdf')
+});
